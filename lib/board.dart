84a5231008d73337fbb25a5ff0b9035b262845e8
L Lesches <levilesches@gmail.com>
2019-03-25 17:33:47 -0400

    Board class

---
+++ lib/board.dart
@@ -0,0 +1,33 @@
+enum Player {X, O}
+
+class Board {
+	static const List<List<int>> candidates = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	final List<Player> board = List.filled (9, null, growable: false);
+	Player turn = Player.X;
+	Player get next => turn == Player.X ? Player.O : Player.X;
+
+	Player get winner {
+		for (final Player player in Player.values) {
+			for (final List<int> candidate in candidates) {
+				if (candidate.every ((int index) => board [index] == player))
+					return player;
+			}
+		}
+		return null;
+	}
+
+	void move (int index) {
+		board [index] = turn;
+		turn = next;
+	}
+}
