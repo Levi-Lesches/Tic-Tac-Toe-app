7d449e72886048787136fa196fdc5d13d5277181
L Lesches <levilesches@gmail.com>
2019-03-27 11:59:34 -0400

    Negamax implementation

--- lib/ai.dart
+++ lib/ai.dart
@@ -1,22 +1,32 @@
 import "board.dart";
 
 class AI {
-	final Board board;
-	AI (this.board);
+	final Board currentBoard;
+	AI (this.currentBoard);
 
 	int evaluate (Board board) {
 		final Victory victory = board.victory;
 		if (victory == null) return null;
 		else if (victory.winner == null) return 0;
-		else if (victory.winner == this.board.turn) return 1;
-		else if (victory.winner == this.board.next) return -1;
-		return null;
+		else if (victory.winner == currentBoard.turn) return 1;
+		else if (victory.winner == currentBoard.next) return -1;
+		else throw "Invalid board configuration";
 	} 
 
-	// int get bestMove {
-	// 	for (final int moveIndex in board.availableMoves) {
-	// 		final Board copy = board.getDummy(moveIndex);
+	// int get bestMove { return negamax ()
+	// 	for (final int moveIndex in currentBoard.availableMoves) {
+	// 		final Board copy = currentBoard.getDummy(moveIndex);
 
 	// 	}
 	// }
+
+	int negamax(Board board) {
+		num result = double.negativeInfinity;
+		for (final int moveIndex in board.availableMoves) {
+			final Board copy = board.getDummy(moveIndex);
+			final int score = evaluate (copy) ?? -negamax (copy);
+			if (score > result) result = score;
+		}
+		return result;
+	}
 }
